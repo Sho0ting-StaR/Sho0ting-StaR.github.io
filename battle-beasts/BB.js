cbc48693e0b5e589a4fcb278c61758f87b317abf
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-10-05 12:29:57 -0600

    BB started

--- battle-beasts/BB.js
+++ battle-beasts/BB.js
@@ -5,15 +5,30 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let yourTeam = {
+  one: "blank",
+  two: "blank",
+  three: "blank",
+};
+
+let space = {
+  name: "non1",
+  speed: "non2",
+  strength: "non3",
+  defense: "non4",
+  health: "non5",
+  type: "non6",
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  yourTeam.one = structuredClone(space);
 }
 
 function draw() {
-  linesSS();
+  background(25);
+  console.log(yourTeam);
 }
 
-function lineSS(){
-  
-}
+
+
