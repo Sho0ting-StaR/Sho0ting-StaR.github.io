ba136707e7c12148dd3aa20aaead3f61d0671937
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-10-16 12:31:36 -0600

    renamed stats

--- battle-beasts/BB.js
+++ battle-beasts/BB.js
@@ -5,99 +5,99 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let yourTeam = ["1","2","3"];
+let YT = ["1","2","3"];
 let slots = [1,2,3];
 let start = true;
-let activSlot = [yourTeam[0],yourTeam[1],yourTeam[2]];
+let activSlot = [YT[0],YT[1],YT[2]];
 let x=0;
 let n=0;
 let actions=3;
 let yourTurn = true;
 let switchmenu=true;
 
-let enemyTeam = ["1","2","3"];
+let ET = ["1","2","3"];
 
 
 let space = {
   name: "non1",
-  speed: "non2",
-  strength: "non3",
-  defense: "non4",
-  health: "non5",
-  Mhealth: "non6",
+  spd: "non2",
+  str: "non3",
+  def: "non4",
+  hp: "non5",
+  Mhp: "non6",
   col: "white",
   stat: [],
 };
 
 let Q = {
   name: "Q",
-  speed: 98,
-  strength: 1.21,
-  defense: 48,
-  health: 500,
-  Mhealth: 500,
+  spd: 98,
+  str: 1.21,
+  def: 48,
+  hp: 500,
+  Mhp: 500,
   col: "purple",
   stat: ["normal"],
 };
 
 let nice = {
   name: "Q",
-  speed: 210,
-  strength: 0.6,
-  defense: 10,
-  health: 10,
-  Mhealth: 10,
+  spd: 210,
+  str: 0.6,
+  def: 10,
+  hp: 10,
+  Mhp: 10,
   col: "red",
   stat: ["not like other guys"],
 };
 
 let StaR = {
   name: "StaR",
-  speed: 78,
-  strength: 1.15,
-  defense: 160,
-  health: 300,
-  Mhealth: 300,
+  spd: 78,
+  str: 1.15,
+  def: 160,
+  hp: 300,
+  Mhp: 300,
   col: "pink",
   stat: ["normal"],
 };
 
 let DizZy = {
   name: "DizZy",
-  speed: 113,
-  strength: 1.15,
-  defense: 30,
-  health: 270,
-  Mhealth: 270,
+  spd: 113,
+  str: 1.15,
+  def: 30,
+  hp: 270,
+  Mhp: 270,
   col: "yellow",
   stat: ["normal"],
 };
 
 let pHoenix = {
   name: "pHoenix",
-  speed: 113,
-  strength: 1.15,
-  defense: 30,
-  health: 270,
-  Mhealth: 270,
+  spd: 113,
+  str: 1.15,
+  def: 30,
+  hp: 270,
+  Mhp: 270,
   col: "orange",
   stat: ["fire"],
 };
 
 let catalog = [Q,nice,StaR,DizZy];
 
-let active = yourTeam[0];
-let Eactive = enemyTeam[0];
+let active = YT[0];
+let Eactive = ET[0];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  yourTeam[0] = structuredClone(space);
-  yourTeam[1] = structuredClone(space);
-  yourTeam[2] = structuredClone(space);
+  YT[0] = structuredClone(space);
+  YT[1] = structuredClone(space);
+  YT[2] = structuredClone(space);
 
-  for(let i=0;i < enemyTeam.length;i++){
-    enemyTeam[i] = structuredClone(random(catalog));
-    console.log(enemyTeam[i]);
+  for(let i=0;i < ET.length;i++){
+    ET[i] = structuredClone(random(catalog));
+    console.log(ET[i]);
   }
 
   // Eactive = enemyTeam[random(enemyTeam)];
@@ -134,34 +134,34 @@ function draw() {
 function assignTeam(n){
   if(switchmenu){
     if(keyIsDown(81)){ // q
-      yourTeam[x-1] = structuredClone(Q);
+      YT[x-1] = structuredClone(Q);
     }
     else if(keyIsDown(69)){ // e
-      yourTeam[x-1] = structuredClone(nice);
+      YT[x-1] = structuredClone(nice);
     }
     else if(keyIsDown(83)){ // s
-      yourTeam[x-1] = structuredClone(StaR);
+      YT[x-1] = structuredClone(StaR);
     }
     else if(keyIsDown(68)){ // d
-      yourTeam[x-1] = structuredClone(DizZy);
+      YT[x-1] = structuredClone(DizZy);
     }
   }
 }
 function offSprites () {
   if(!switchmenu){
-    fill(yourTeam[0].col);
+    fill(YT[0].col);
     rect(width-75,height-25,20,20);
-    fill(yourTeam[1].col);
+    fill(YT[1].col);
     rect(width-50,height-25,20,20);
-    fill(yourTeam[2].col);
+    fill(YT[2].col);
     rect(width-25,height-25,20,20);
   }
   else if(switchmenu){
-    fill(yourTeam[0].col);
+    fill(YT[0].col);
     rect(width/2-75,height/2,60,60);
-    fill(yourTeam[1].col);
+    fill(YT[1].col);
     rect(width/2,height/2,60,60);
-    fill(yourTeam[2].col);
+    fill(YT[2].col);
     rect(width/2+75,height/2,60,60);
     fill(200,20,50);
     rect(width-80,0,80,80);
@@ -179,11 +179,11 @@ function mouseClicked(){
   }
 
   if(!switchmenu&&yourTurn&&mouseY>height-150){
-    let damage = 25;
-    if(random(yourTeam[active-1].speed/2,yourTeam[active-1].speed)>random(enemyTeam[Eactive-1].speed/2,enemyTeam[Eactive-1].speed)){
-      let totaldmg = yourTeam[active-1].strength * damage - enemyTeam[Eactive-1].defense/2;
+    let dmg = 25;
+    if(random(YT[active-1].spd/2,YT[active-1].spd)>random(ET[Eactive-1].spd/2,ET[Eactive-1].spd)){
+      let totaldmg = YT[active-1].str * dmg - ET[Eactive-1].def/2;
       if(totaldmg>0){
-        enemyTeam[Eactive-1].health-= totaldmg;
+        ET[Eactive-1].hp-= totaldmg;
         yourTurn = false;
       }
     }
@@ -235,46 +235,46 @@ function arena(){
       rect(width/5,height-200,width/5,100);
     }
     noStroke();
-    console.log(enemyTeam[Eactive-1].health);
+    console.log(ET[Eactive-1].health);
   }
 }
 function drawBeasts(){
   if(!start&&!switchmenu){
-    fill(yourTeam[active-1].col); // sprite
+    fill(YT[active-1].col); // sprite
     rect(width/3-50,height/1.5-80,120,120);
 
     fill(60); // healthbar
     rect(width-100,height-100,100,30);
-    rect(width-100,height-100,100*yourTeam[active-1].health/yourTeam[active-1].Mhealth,30);
-    fill(yourTeam[active-1].col);
+    rect(width-100,height-100,100*YT[active-1].hp/YT[active-1].Mhp,30);
+    fill(YT[active-1].col);
 
-    rect(width-100,height-100,100*yourTeam[active-1].health/yourTeam[active-1].Mhealth,30);
+    rect(width-100,height-100,100*YT[active-1].hp/YT[active-1].Mhp,30);
 
 
-    fill(enemyTeam[Eactive-1].col); // enemy sprite
+    fill(ET[Eactive-1].col); // enemy sprite
     rect(width-width/2.55+50,height/3-60,80,80);
 
     fill(60); // enemy healthbar
     rect(0,100,100,30);
-    fill(enemyTeam[Eactive-1].col);
-    rect(0,100,100*enemyTeam[Eactive-1].health/enemyTeam[Eactive-1].Mhealth,30);
-    text(enemyTeam[Eactive-1].health,0, 180);
+    fill(ET[Eactive-1].col);
+    rect(0,100,100*ET[Eactive-1].hp/ET[Eactive-1].Mhp,30);
+    text(ET[Eactive-1].hp,0, 180);
   }
 }
 
 function battle(){ // drawing arena details
   if(!start&&!switchmenu){
     if(!yourTurn){
-      if(enemyTeam[Eactive-1].health<=0){
-        enemyTeam.splice(enemyTeam.indexOf[Eactive-1],1);
-        Eactive = enemyTeam[random(0,enemyTeam.length)];
+      if(ET[Eactive-1].hp<=0){
+        ET.splice(ET.indexOf[Eactive-1],1);
+        Eactive = ET[random(0,ET.length)];
       }
       else{
-        let damage = 30;
-        if(random(enemyTeam[Eactive-1].speed/2,enemyTeam[Eactive-1].speed)>random(yourTeam[active-1].speed/2,yourTeam[active-1].speed)){
-          let totaldmg = enemyTeam[Eactive-1].strength * damage - yourTeam[active-1].defense/2;
+        let dmg = 20;
+        if(random(ET[Eactive-1].spd/2,ET[Eactive-1].spd)>random(YT[active-1].spd/2,YT[active-1].spd)){
+          let totaldmg = ET[Eactive-1].str * dmg - YT[active-1].def/2;
           if(totaldmg>0){
-            yourTeam[active-1].health-= totaldmg;
+            YT[active-1].hp-= totaldmg;
             yourTurn = true;
           }
         }
