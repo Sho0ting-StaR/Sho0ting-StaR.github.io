357486ef8e90ff4535a7d4b41f46dd3a169ea0a6
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-10-30 12:16:54 -0600

    villeJs spawners

--- pixelville/ville.js
+++ pixelville/ville.js
@@ -24,9 +24,13 @@ function setup() {
 }
 
 function draw() {
-  if(frameCount%12===0){
+  if(frameCount%12===0){ // equation for water shimmer
     showGrid(grid);
   }
+  if(frameCount%8===0){
+    //moveFolk();
+  }
+  displayFolk(peeps);
 }
 
 function mouseClicked(){
@@ -34,18 +38,52 @@ function mouseClicked(){
   let y = Math.floor(mouseY/tilesize);
   if(grid[y][x]===0){
     grid[y][x]=3;
-    grassCheck(); // to make peeps
+    grassCheck(grid,x,y); // to make peeps
   }
 }
 
-function grassCheck(array,lr,ud){
-  let x = Math.floor(lr/tilesize);
-  let y = Math.floor(ud/tilesize);
-  for(let i = -1;i<2;i++){
-    for(let j = -1;j<2;j++){ // check for neighbouring "0" grass tiles,
+function displayFolk(array){ // draw peeps
+  for(let y= 0;y < array.length;y++){
+    if(array[y][3]===1){ //live
+      noStroke();
+      fill(120,0,70);
+      rect(array[y][0]*tilesize, array[y][1]*tilesize, array[y][2], array[y][2]);
+    }
+    else if(array[y][3]===0){//dead
+      noStroke();
+      fill(60,0,28);
+      rect(array[y][0]*tilesize, array[y][1]*tilesize, array[y][2], array[y][2]);
+    }
+    
+  }
+}
 
+function moveFolk(array){
+  for(let y= 0;y < array.length;y++){
+    let m = random(-1,2);
+    let x = random(0,2);
+    array[y][x] -= m*5;
+    if(Math.floor(array[y][x])!==0){
+      array[y][x] +=m*5;
     }
   }
+  
+}
+
+function grassCheck(array,lr,ud){
+  let x = lr; //Math.floor(lr/tilesize);
+  let y = ud; //Math.floor(ud/tilesize);
+  for(let i = -1;i<2;i++){
+    for(let j = -1;j<2;j++){ // check for neighbouring "0" grass tiles,
+      if(array[y-i][x-j]===0){
+        if(!(i===1&&j===i)){
+          peeps.push([x-j,y-i,random(12,21),1]);
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
 function makeGrid(array){
@@ -64,7 +102,6 @@ function showGrid(array){ // building the map
         noStroke();
         fill(0,80,0);
         rect(x*tilesize,y*tilesize,tilesize,tilesize);
-        console.log(x*tilesize,y*tilesize,tilesize,tilesize);
       }
       else if(array[y][x]===1){//forest
         noStroke();
@@ -73,7 +110,7 @@ function showGrid(array){ // building the map
       }
       else if(array[y][x]===2){//water
         noStroke();
-        fill(0,0,random(60,91));
+        fill(0,0,random(110,131));
         rect(x*tilesize,y*tilesize,tilesize,tilesize);
       }
       else if(array[y][x]===3){//houses
@@ -86,7 +123,6 @@ function showGrid(array){ // building the map
         fill(60,40,40);
         rect(x*tilesize,y*tilesize,tilesize,tilesize);
       }
-
     }
   }
 }
