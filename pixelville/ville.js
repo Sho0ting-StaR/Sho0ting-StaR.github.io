e7b12fd92ae153d0cb3fdcc645692a26826925c9
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-11-01 12:16:51 -0600

    ville. js checksurrounds

--- pixelville/ville.js
+++ pixelville/ville.js
@@ -6,21 +6,24 @@
 // - describe what you did to take this project "above and beyond"
 
 let grid = [];
-let mapSizeY = 24;
-let mapSizeX = 30;
+let mapSizeY = 36;
+let mapSizeX = 80;
 let tilesize;
 let peeps = [];
+let traversable = [0,4,5];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   if(mapSizeX>mapSizeY){
     tilesize = windowHeight/mapSizeY;
+    // mapSizeX = Math.floor(windowWidth/tilesize);
   }
   else{
     tilesize = windowWidth/mapSizeX;
   }
   makeGrid(grid);
   background(40);
+  console.log(grid);
 }
 
 function draw() {
@@ -47,37 +50,58 @@ function displayFolk(array){ // draw peeps
     if(array[y][3]===1){ //live
       noStroke();
       fill(120,0,70);
-      rect(array[y][0]*tilesize, array[y][1]*tilesize, array[y][2], array[y][2]);
+      rect(array[y][0], array[y][1], array[y][2], array[y][2]); // [y][0] and [y][1] are normally *tilesize
     }
     else if(array[y][3]===0){//dead
       noStroke();
       fill(60,0,28);
-      rect(array[y][0]*tilesize, array[y][1]*tilesize, array[y][2], array[y][2]);
+      rect(array[y][0], array[y][1], array[y][2], array[y][2]);
     }
     
   }
 }
 
 function moveFolk(array){
-  let nextFrame = [];
+  let nextFrame = array;
   for(let y= 0;y < array.length;y++){
-    nextFrame.push([]);
     for(let z=0;z<array[y].length;z++){
-      let m = random(-1,2);
+      let m = Math.floor(random(-1,2));
       let x = random(100);
-      // nextFrame[y][x] -= m*5;
-      if(x > 50){
-        if(Math.floor(array[y][z]-m*5)!==0){
-          nextFrame[y].push(nextFrame[y][z] +=m*5);
+      if(!array[y][3]===0){
+        if(x > 50){
+          if(bactpos(y,)+m*1===0){  // give bactpos m
+            //grid[Math.floor(array[y][1]/tilesize)][Math.floor(array[y][0]/tilesize)] OR grid[y][0] old
+            nextFrame[y][0] +=m*1;
+          }
         }
-        else{
-          nextFrame[y].push(nextFrame[y][z] -=m*5);
+        if(x < 50){
+          if(grid[Math.floor(array[y][1]/tilesize)][Math.floor(array[y][0]/tilesize)]+m*1===0){
+            nextFrame[y][1] +=m*1;
+          }
         }
-        array = nextFrame;
+      }
+    }
+    array = nextFrame;
+  }
+}
+
+function bactpos(bact){ // FINISH LATER
+  let x = peeps[bact][0];
+  let y = peeps[bact][1];
+  for(let i = -1;i<2;i++){
+    for(let j = -1;j<2;j++){ // check for neighbouring "0" grass tiles,
+      if(grid[y-i][x-j]===0){
+        if(!(i===1&&j===i)){
+          let px = x+j;let py = y-i;
+          peeps.push([px*tilesize,py*tilesize,tilesize/4,1]);
+          return true;
+        }
       }
     }
   }
+  return false;
 }
+
 
 function grassCheck(array,lr,ud){
   let x = lr; //Math.floor(lr/tilesize);
@@ -86,7 +110,8 @@ function grassCheck(array,lr,ud){
     for(let j = -1;j<2;j++){ // check for neighbouring "0" grass tiles,
       if(array[y-i][x-j]===0){
         if(!(i===1&&j===i)){
-          peeps.push([x-j,y-i,random(12,21),1]);
+          let px = x+j;let py = y-i;
+          peeps.push([px*tilesize,py*tilesize,tilesize/4,1]);
           return true;
         }
       }
