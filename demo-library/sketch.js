c47dd593f335e4e80db3a2c97c613c203d7c0f79
Sho0ting-StaR <146768151+Sho0ting-StaR@users.noreply.github.com>
2023-11-27 11:35:37 -0600

    demo lib

---
+++ demo-library/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+// just click to make a new p5 project and instead lookup contributor library and choose one
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
